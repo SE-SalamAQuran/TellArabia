e6a97929bc987e014c8dfab0f218966fd09ef65c
SE-SalamAQuran <salamquraan789@gmail.com>
2021-11-25 13:06:51 +0200

    Fixed error in services

--- controllers/service.controllers.js
+++ controllers/service.controllers.js
@@ -315,24 +315,12 @@ module.exports = {
                             }
                         }
                     })
-                    .populate({
-                        path: 'sub_categories',
-                        populate: {
-                            path: 'offers',
-                            populate: {
-                                path: 'addedBy',
-                                populate: {
-                                    path: "userInfo"
-                                }
-                            }
-                        }
-                    })
                     .then((result) => {
 
                         return res.status(200).json({ "success": true, "result": result });
 
                     }).catch((error) => {
-                        return res.status(400).json({ "success": false, "message": "Unable to find services" });
+                        return res.status(400).json({ "success": false, "message": "Unable to find services", "error": error });
 
                     });
             }
